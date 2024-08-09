d75bb4e3e3e961dd222c3c9408fab80c963a0062
Aaron On'osala <164625563+AaronOnosala@users.noreply.github.com>
2024-08-09 08:09:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# importing libraries
+# Import the YOLO library from the ultralytics module
 from ultralytics import YOLO
 
-# loading the model
+# Initialize the YOLO model with a pre-trained classification model
 model = YOLO('yolov8l-cls.pt')
+
+# Train the YOLO model with the specified parameters
+# - data: Path to the directory containing the maize classification dataset
+# - epochs: Number of training epochs to run (100 in this case)
+# - imgsz: Size of the input images for training (640x640 pixels)
 model.train(data='/Users/aarononosala/Documents/Makerere/Classification_maize',
             epochs=100, imgsz=640)
